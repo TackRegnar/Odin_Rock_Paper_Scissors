41eb584de6783e2c57439380e11e43eb3ff30711
TackRegnar <TackRegnar@protonmail.com>
2022-06-23 04:35:14 -0500

    Basic UI with round results

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function computerPlay() {
-  let x = random(1,3);
+  let x = random(1, 3);
 
   switch (x) {
     case 1:
       return "rock";
       break;
-      case 2:
-        return "paper";
-        break;
-      case 3:
-        return "scissors";
-        break;
-      default:
-        return "error";
+    case 2:
+      return "paper";
+      break;
+    case 3:
+      return "scissors";
+      break;
+    default:
+      return "error";
   }
 }
 
@@ -20,7 +20,8 @@ function random(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function play_round(playerSelection,computerSelection) {
+function play_round(playerSelection, computerSelection) {
+
   if (playerSelection === "rock" && computerSelection === "rock") {
     return "It's a Tie!";
   } else if (playerSelection === "rock" && computerSelection === "paper") {
@@ -45,11 +46,30 @@ function play_round(playerSelection,computerSelection) {
 }
 
 function game() {
-  let playerSelection = ""
-  for (let i = 0; i < 5; i++) {
-    playerSelection = window.prompt('Choose: Rock, Paper, Scissors: ');
-    console.log(play_round(playerSelection.toLowerCase(),computerPlay()));
-  }
+  //buttons
+  const rock_btn = document.getElementById('rock_btn');
+  const paper_btn = document.getElementById("paper_btn");
+  const scissors_btn = document.getElementById("scissors_btn");
+  //show round results
+  const round_result_div = document.getElementById("round_result_div");
+  const round_result = document.createElement("p");
+  //show end game results
+  const end_game_msg_div = document.getElementById("end_game_msg_div");
+  const end_game_msg = document.createElement("p");
+  round_result_div.appendChild(round_result);
+  end_game_msg_div.appendChild(end_game_msg); // TODO add showing scores and who wins when it reachs 5 rounds played
+
+  rock_btn.addEventListener('click', () => {
+    round_result.textContent = play_round("rock", computerPlay());
+  });
+
+  paper_btn.addEventListener('click', () => {
+    round_result.textContent = play_round("paper", computerPlay());
+  });
+
+  scissors_btn.addEventListener('click', () => {
+    round_result.textContent = play_round("scissors", computerPlay());
+  });
 }
 
 game();
